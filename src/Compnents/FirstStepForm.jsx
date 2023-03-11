a54bcc7b70abbbfd8d250183d3d5316ed40ec65a
deeppatel007 <deepp0254@gmail.com>
2023-03-12 00:16:12 +0530

    width changes

--- src/Compnents/FirstStepForm.jsx
+++ src/Compnents/FirstStepForm.jsx
@@ -56,31 +56,31 @@ const FirstStepForm = ({ jobData, setJobData }) => {
         <div class="bg-white shadow-md rounded-10 px-8 py-8 h-141 w-145 ">
             <div>
               <span class="display-block top-3">
-                <span class="float-right right-0 text-base bold text-base">step 1</span>
+                <span class="float-right right-0 text-base bold text-base leading-6">step 1</span>
               </span>
             </div>
             <div class="text-xl">
-              Create a Job
+              <span class="w-28 leading-7 text-xl">Create a Job</span>
               <div class= "pt-6">
-                <label class="block text-sm pb-1 font-medium text-gray-900 " >Job title<span className="text-red-600">*</span>
+                <label class="block text-sm mb-1 font-medium text-gray-900 " ><span class="w-16">Job title</span><span className="text-red-600">*</span>
                 {
                   isEmptyTitle && <span class="text-red-500 text-xs pl-1">This field is required</span>
                 }
                 </label>
-                <input type="text" class=" font-normalh-9 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="ex. UX UI Designer" name="jobTitle" value={jobData.jobTitle} onChange={(e) => handleOnChange(e)} required />
+                <input type="text" class=" font-normal  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block w-full h-9 p-2.5 " placeholder="ex. UX UI Designer" name="jobTitle" value={jobData.jobTitle} onChange={(e) => handleOnChange(e)} required />
                 
               </div>
               <div class = "pt-6">
-                <label class="pb-1 font-normal block text-sm font-medium text-gray-900 ">Company name<span className="text-red-600">*</span>
+                <label class="mb-1 font-medium  block text-sm font-medium text-gray-900 "><span class="w-123">Company name</span><span className="text-red-600">*</span>
                 {
                   isEmptyCompanyName && <span class="text-red-500 text-xs pl-1">This field is required</span>
                 }
                 </label>
-                <input type="text" class=" h-9 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="ex. Google" name="companyName" value={jobData.companyName} onChange={(e) => handleOnChange(e)} required />
+                <input type="text" class="font-normal h-9 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="ex. Google" name="companyName" value={jobData.companyName} onChange={(e) => handleOnChange(e)} required />
             
               </div>
               <div class = "pt-6">
-                <label class="pb-1 block text-sm font-medium text-gray-900 ">Industry<span className="text-red-600">*</span>
+                <label class="mb-1 block text-sm  font-medium text-gray-900 "><span class="w-65">Industry</span><span className="text-red-600">*</span>
                 {
                   isEmptyIndustryName && <span class="text-red-500 text-xs pl-1">This field is required</span>
                 }
@@ -89,15 +89,15 @@ const FirstStepForm = ({ jobData, setJobData }) => {
               </div>
               <div class="grid gap-6 md:grid-cols-2 pt-6">
                 <div>
-                  <label class="pb-1 font-normal block text-sm font-medium text-gray-900 ">Location</label>
+                  <label class="mb-1 w-60 block text-sm font-medium text-gray-900 ">Location</label>
                   <input type="text" class=" font-normal h-9 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="ex. Chennai" name="locationName" value={jobData.locationName} onChange={(e) => handleOnChange(e)} />
                 </div>
                 <div>
-                  <label class="pb-1 block text-sm font-medium text-gray-900 ">Remote Type</label>
+                  <label class="mb-1 block text-sm w-24 font-medium text-gray-900 ">Remote Type</label>
                   <input type="text" class="font-normal h-9 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 " placeholder="ex. in-office" name="remoteType" value={jobData.remoteType} onChange={(e) => handleOnChange(e)} />
                 </div>
               </div>
-              <button class="flex items-end justify-end bottom-0 mt-24 float-right h-10 W-17 inline-flex justify-center rounded-5 bg-sky-500 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 w-16" name="pageNumber" onClick={(e) => secondStep(e)}>Next</button>
+              <button class="flex items-end w-68 justify-end  bottom-0 mt-24 float-right h-10  inline-flex justify-center rounded-5 bg-sky-500 py-2 px-3 text-base  text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 text-base" name="pageNumber" onClick={(e) => secondStep(e)}>Next</button>
             
               </div>
               
--- src/Compnents/SecondStepForm.jsx
+++ src/Compnents/SecondStepForm.jsx
@@ -18,20 +18,19 @@ const SecondStepForm = ({ jobData, setJobData }) => {
     return (
         <>
             {
-                showJobs !== true ? 
+                showJobs !== true ?
                     <div class="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
                         <div class="bg-white shadow-md rounded-10 px-8 py-8 h-141 w-145 ">
                         <div>
-              <span class="display-block top-3">
-                <span class="float-right right-0 text-base bold text-base">step 2</span>
-              </span>
-            </div>
-                            <div class="text-xl">
-                            <p class="leading-7 w-96">
-                                Create a Job
-                                </p>
-                                <div class= "pt-6">
-                                    <label class="pb-1 block text-sm font-medium text-gray-900 ">Experience</label>
+            <span class="display-block top-3">
+              <span class="float-right right-0 text-base bold text-base leading-6">step 2</span>
+            </span>
+          </div>
+          <div class="text-xl">
+            <span class="w-28 leading-7 text-xl">Create a Job</span>
+
+                                <div class="pt-6">
+                                    <label class=" block text-sm font-medium text-gray-900 w-77">Experience</label>
                                     <div class="grid gap-6 md:grid-cols-2 pt-1">
                                         <div>
                                             <input type="text" class="font-normal bg-gray-50 w-full h-9 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block  p-2.5 " name="minimumExperience" value={jobData.minimumExperience} onChange={(e) => { handleOnChange(e) }} placeholder="Minimum" />
@@ -41,8 +40,8 @@ const SecondStepForm = ({ jobData, setJobData }) => {
                                         </div>
                                     </div>
                                 </div>
-                                <div class= "pt-6">
-                                    <label class="pb-1 block text-sm font-medium text-gray-900 " >Salary</label>
+                                <div class="pt-6">
+                                    <label class=" block text-sm font-medium text-gray-900 w-45 " >Salary</label>
                                     <div class="grid gap-6 md:grid-cols-2 pt-1">
                                         <div>
                                             <input type="text" class="font-normal bg-gray-50 w-full h-9 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block  p-2.5 " name="minimumSalary" value={jobData.minimumSalary} onChange={(e) => { handleOnChange(e) }} placeholder="Minimum" />
@@ -51,35 +50,35 @@ const SecondStepForm = ({ jobData, setJobData }) => {
                                             <input type="text" class="font-normal bg-gray-50 w-full h-9 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block p-2.5 " name="maximumSalary" value={jobData.maximumSalary} onChange={(e) => { handleOnChange(e) }} placeholder="Maximum" />
                                         </div>
                                     </div>
-                                    
+
                                 </div>
                                 <div class="pt-6">
-                                    <label class="pb-1 block  text-sm font-medium text-gray-900" >Total Employee</label>
+                                    <label class=" block  text-sm font-medium text-gray-900 w-110" >Total Employee</label>
                                     <div class="pt-1">
-                                    <input type="text" class="font-normal bg-gray-50 w-full h-9 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block  p-2.5 " name="numberOfEmployees" value={jobData.numberOfEmployees} onChange={(e) => { handleOnChange(e) }} placeholder="ex. 100" />
+                                        <input type="text" class="font-normal bg-gray-50 w-full h-9 border border-gray-300 text-gray-900 text-sm rounded-5 focus:ring-blue-500 focus:border-blue-500 block  p-2.5 " name="numberOfEmployees" value={jobData.numberOfEmployees} onChange={(e) => { handleOnChange(e) }} placeholder="ex. 100" />
                                     </div>
-                                    </div>
-                            </div>
-                            <div class= "pt-6">
-                            <label class="block text-sm font-medium text-gray-900 " >Apply type</label>
-                            <div class="flex gap-4 pt-3">
-                                <div class="flex items-center">
-                                    <input type="radio" value="QuickApply" name="applyType" onChange={(e) => { handleOnChange(e) }} class="font-normal w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  " />
-                                    <label class="ml-1 text-sm font-medium text-gray-900 ">Quick Apply</label>
-                                </div>
-                                <div class="flex items-center block">
-                                    <input type="radio" name="applyType" value="ExternalApply" onChange={(e) => { handleOnChange(e) }} class="font-normal w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" />
-                                    <label class="ml-1 text-sm font-medium text-gray-900 ">External apply</label>
                                 </div>
                             </div>
+                            <div class="pt-6">
+                                <label class="block text-sm font-medium text-gray-900 w-76" >Apply type</label>
+                                <div class="flex gap-4 pt-3">
+                                    <div class="flex items-center">
+                                        <input type="radio" value="QuickApply" name="applyType" onChange={(e) => { handleOnChange(e) }} class="font-normal w-5 h-5 text-blue-600 bg-gray-100 border-gray-300  " />
+                                        <label class="ml-1 text-sm font-medium text-gray-900 ">Quick Apply</label>
+                                    </div>
+                                    <div class="flex items-center block pl-1">
+                                        <input type="radio" name="applyType" value="ExternalApply" onChange={(e) => { handleOnChange(e) }} class="font-normal w-5 h-5 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500" />
+                                        <label class="ml-1 text-sm font-medium text-gray-900 ">External apply</label>
+                                    </div>
+                                </div>
                             </div>
                             <div class=" float-right pt-24">
-                                <button type="submit" class="inline-flex justify-center rounded-md bg-sky-500 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 w-16 border-2" onClick={sendJobData}>Save</button>
+                                <button type="submit" class="w-72 h-10 inline-flex justify-center font-medium rounded-md bg-sky-500 py-2 px-3 text-base	 text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400  border-2" onClick={sendJobData}>Save</button>
                             </div>
                         </div>
                     </div>
                     :
-                    <Jobs applyType={jobData.applyType}/>
+                    <Jobs applyType={jobData.applyType} />
 
             }
         </>
